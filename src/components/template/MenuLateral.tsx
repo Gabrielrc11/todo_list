5dfbf0ba2a7efebc21ef6cbe02fb41c669148ce0
Gabriel Carvalho <gabrielhenriquerc11@gmail.com>
2024-09-14 03:54:57 -0300

    feat: auth

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,12 +1,32 @@
-import { IconeAjustes, IconeCasa } from "../icons";
+import { IconeAjustes, IconeCasa, IconeSair } from "../icons";
+import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function MenuLateral() {
     return (
-        <aside>
-            <ul>
+        <aside className={`
+            flex flex-col
+            bg-gray-200 dark:text-gray-700
+            dark:bg-gray-900 
+        `}>
+            <div className={`
+                flex flex-col items-center justify-center
+                bg-gradient-to-r from-indigo-500 to-purple-800
+                h-20 w-20
+            `}>
+                <Logo />
+            </div>
+            <ul className="flex-grow">
                 <MenuItem url="/" texto="Início" icone={IconeCasa} />
                 <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
+            </ul>
+            <ul>
+                <MenuItem onClick={() => console.log('logout')} texto="Sair" icone={IconeSair} className={`
+                    text-red-600 dark:text-red-400
+                    hover:bg-red-400 hover:text-white
+                    dark:hover:text-white
+                `} 
+                />
             </ul>
         </aside>
     )
---
+++ src/pages/ajustes.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/template/Layout";
+
+export default function Ajustes() {
+
+  return (
+    <Layout titulo="Ajustes e Configurações" subtitulo="Aplique configurações personalizadas aqui" >
+      <h1>Ajustes</h1>
+    </Layout>
+  );
+}
