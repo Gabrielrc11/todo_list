2b4cdbcbf30e199184a8ef0ef60f949849eb3186
Gabriel Carvalho <gabrielhenriquerc11@gmail.com>
2024-09-27 13:53:10 -0300

    fix: color auth input

--- src/components/auth/Authinput.tsx
+++ src/components/auth/Authinput.tsx
@@ -15,6 +15,7 @@ export default function AuthInput(props: AuthInputProps) {
                 px-4 py-3 rounded-lg bg-gray-200 mt-2
                 border focus:border-blue-500 focus:bg-white
                 focus:outline-none
+                text-gray-900
             `}
             />
         </div>
